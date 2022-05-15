ccb7e8c4dd3d2418b5607e540beba447c1eef6f6
Dan Yazovsky <daniil.yazovsky@gmail.com>
2022-05-15 16:11:42 +0100

    Defining scraper models

---
+++ sneakpeek/lib/models.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class ScraperSchedule(str, Enum):
+    INACTIVE = "inactive"
+    EVERY_HOUR = "every_hour"
+    EVERY_DAY = "every_day"
+    EVERY_WEEK = "every_week"
+    EVERY_MONTH = "every_month"
+    CRONTAB = "crontab"
+
+
+class ScraperRunPriority(Enum):
+    UTMOST = 0
+    HIGH = 1
+    NORMAL = 2
+
+
+class ScraperRunStatus(str, Enum):
+    PENDING = "pending"
+    STARTED = "started"
+    FAILED = "failed"
+    SUCCEEDED = "succeeded"
+    DEAD = "dead"
+    KILLED = "killed"
+
+
+class Scraper(BaseModel):
+    id: int
+    name: str
+    schedule: ScraperSchedule
+    schedule_crontab: str | None
+    handler: str
+    config: str
+
+
+class ScraperRun(BaseModel):
+    id: int
+    scraper: Scraper
+    status: ScraperRunStatus
+    priority: ScraperRunPriority
+    created_at: datetime
+    started_at: datetime | None = None
+    last_active_at: datetime | None = None
+    finished_at: datetime | None = None
+    result: str | None = None
+
+
+class Lease(BaseModel):
+    name: str
+    owner_id: str
+    acquired: datetime
+    acquired_until: datetime
