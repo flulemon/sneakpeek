632c85b1210efec1eb96fe2bc1151fcc90fc10f3
Dan Yazovsky <daniil.yazovsky@gmail.com>
2022-05-15 19:52:03 +0100

    Defining storage interface

---
+++ sneakpeek/lib/storage/base.py
@@ -0,0 +1,91 @@
+from abc import ABC
+from datetime import datetime
+from typing import List
+
+import fastapi_jsonrpc as jsonrpc
+
+from sneakpeek.lib.models import Lease, Scraper, ScraperRun
+
+
+class ScraperNotFoundError(jsonrpc.BaseError):
+    CODE = 5000
+    MESSAGE = "Scraper not found"
+
+
+class ScraperRunNotFoundError(jsonrpc.BaseError):
+    CODE = 5001
+    MESSAGE = "Scraper run not found"
+
+
+class ScraperRunPingNotStartedError(jsonrpc.BaseError):
+    CODE = 5002
+    MESSAGE = "Failed to ping not started scraper run"
+
+
+class ScraperRunPingFinishedError(jsonrpc.BaseError):
+    CODE = 5002
+    MESSAGE = "Tried to ping finished scraper run"
+
+
+class Storage(ABC):
+    async def search_scrapers(
+        self,
+        name_filter: str | None = None,
+        max_items: int | None = None,
+        last_id: int | None = None,
+    ) -> List[Scraper]:
+        raise NotImplementedError()
+
+    async def get_scrapers(self) -> List[Scraper]:
+        raise NotImplementedError()
+
+    async def get_scraper(self, id: int) -> Scraper:
+        raise NotImplementedError()
+
+    async def maybe_get_scraper(self, id: int) -> Scraper | None:
+        raise NotImplementedError()
+
+    async def create_scraper(self, scraper: Scraper) -> Scraper:
+        raise NotImplementedError()
+
+    async def update_scraper(self, scraper: Scraper) -> Scraper:
+        raise NotImplementedError()
+
+    async def delete_scraper(self, id: int) -> Scraper:
+        raise NotImplementedError()
+
+    async def get_scraper_runs(self, scraper_id: int) -> List[ScraperRun]:
+        raise NotImplementedError()
+
+    async def add_scraper_run(self, scraper_run: ScraperRun) -> ScraperRun:
+        raise NotImplementedError()
+
+    async def update_scraper_run(self, scraper_run: ScraperRun) -> ScraperRun:
+        raise NotImplementedError()
+
+    async def ping_scraper_run(
+        self,
+        scraper_id: int,
+        scraper_run_id: int,
+    ) -> ScraperRun:
+        raise NotImplementedError()
+
+    async def dequeue_scraper_run(self) -> ScraperRun | None:
+        raise NotImplementedError()
+
+    async def delete_old_scraper_runs(self, keep_last: int = 50) -> None:
+        raise NotImplementedError()
+
+    async def get_unfinished_scraper_runs(self, scraper_id: int) -> bool:
+        raise NotImplementedError()
+
+    async def maybe_acquire_lease(
+        self,
+        lease_name: str,
+        owner_id: str,
+        acquire_until: datetime,
+    ) -> Lease | None:
+        raise NotImplementedError()
+
+    async def release_lease(self, lease_name: str, owner_id: str) -> None:
+        raise NotImplementedError()
