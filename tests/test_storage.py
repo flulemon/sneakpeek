3e08d5120a98e93c000704dcd483ab88500e6795
Dan Yazovsky <daniil.yazovsky@gmail.com>
2022-06-14 18:30:02 +0100

    Improving lease test

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -267,7 +267,8 @@ async def test_delete_old_scraper_runs(storage: Storage):
 @pytest.mark.asyncio
 @pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
 async def test_lease(storage: Storage):
-    lease_name = "test_lease"
+    lease_name_1 = "test_lease_1"
+    lease_name_2 = "test_lease_2"
     owner_1 = "owner_id_1"
     owner_2 = "owner_id_2"
     now = datetime.utcnow()
@@ -276,23 +277,28 @@ async def test_lease(storage: Storage):
 
     # initial acquire
     assert (
-        await storage.maybe_acquire_lease(lease_name, owner_1, owner_1_acquire_until)
+        await storage.maybe_acquire_lease(lease_name_1, owner_1, owner_1_acquire_until)
+        is not None
+    )
+    # another lease can be acquired
+    assert (
+        await storage.maybe_acquire_lease(lease_name_2, owner_2, owner_2_acquire_until)
         is not None
     )
     # lock is acquired so no one can acquire
     assert (
-        await storage.maybe_acquire_lease(lease_name, owner_2, owner_2_acquire_until)
+        await storage.maybe_acquire_lease(lease_name_1, owner_2, owner_2_acquire_until)
         is None
     )
     # owner can re-acquire
     assert (
-        await storage.maybe_acquire_lease(lease_name, owner_1, owner_1_acquire_until)
+        await storage.maybe_acquire_lease(lease_name_1, owner_1, owner_1_acquire_until)
         is not None
     )
 
     # lock expires and can be acuired
     await asyncio.sleep(1)
     assert (
-        await storage.maybe_acquire_lease(lease_name, owner_2, owner_2_acquire_until)
+        await storage.maybe_acquire_lease(lease_name_1, owner_2, owner_2_acquire_until)
         is not None
     )
