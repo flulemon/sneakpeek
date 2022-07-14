3b0763d52b00442386361f26bb8ff5076624359c
Dan Yazovsky <daniil.yazovsky@gmail.com>
2022-07-14 17:21:55 +0100

    Updating queue tests

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,7 +1,20 @@
+from datetime import datetime
+
 import pytest
 
-from sneakpeek.lib.models import UNSET_ID, Scraper, ScraperRunPriority, ScraperSchedule
-from sneakpeek.lib.queue import Queue, ScraperHasActiveRunError
+from sneakpeek.lib.models import (
+    UNSET_ID,
+    Scraper,
+    ScraperRunPriority,
+    ScraperRunStatus,
+    ScraperSchedule,
+)
+from sneakpeek.lib.queue import (
+    Queue,
+    ScraperHasActiveRunError,
+    ScraperRunPingFinishedError,
+    ScraperRunPingNotStartedError,
+)
 from sneakpeek.lib.storage.base import Storage
 from sneakpeek.lib.storage.in_memory_storage import InMemoryStorage
 
@@ -61,3 +74,60 @@ async def test_enqueue_count_equals_dequeue_count(queue: Queue, storage: Storage
         dequeued.append((await queue.dequeue()).id)
     assert dequeued == expected
     await queue.dequeue() is None
+
+
+@pytest.mark.asyncio
+async def test_scraper_queue(queue: Queue, storage: Storage):
+    created_scraper = await storage.create_scraper(_get_scraper("test_scraper_queue"))
+    created_run = await queue.enqueue(created_scraper.id, ScraperRunPriority.UTMOST)
+    assert created_run.id >= 0, "Expected scraper run ID to be positive or zero"
+    assert created_run.scraper == created_scraper
+    assert created_run.status == ScraperRunStatus.PENDING
+    actual_run = await storage.get_scraper_runs(created_scraper.id)
+    assert actual_run == [created_run]
+    with pytest.raises(ScraperRunPingNotStartedError):
+        await queue.ping_scraper_run(created_scraper.id, created_run.id)
+    dequeued = await queue.dequeue()
+    assert dequeued.id == created_run.id
+    assert dequeued.status == ScraperRunStatus.STARTED
+    assert await queue.dequeue() is None
+    await queue.ping_scraper_run(dequeued.scraper.id, dequeued.id)
+    dequeued.status = ScraperRunStatus.SUCCEEDED
+    dequeued.finished_at = datetime.utcnow()
+    finished = await storage.update_scraper_run(dequeued)
+    with pytest.raises(ScraperRunPingFinishedError):
+        await queue.ping_scraper_run(finished.scraper.id, finished.id)
+    assert await queue.dequeue() is None
+
+
+@pytest.mark.asyncio
+async def test_scraper_priority_queue_dequeue_order(queue: Queue, storage: Storage):
+    run_utmost_priority_dequeued_1st = await storage.create_scraper(
+        _get_scraper("run_utmost_priority_dequeued_1st")
+    )
+    run_high_priority_dequeued_2nd = await storage.create_scraper(
+        _get_scraper("run_high_priority_dequeued_2nd")
+    )
+    run_high_priority_dequeued_3rd = await storage.create_scraper(
+        _get_scraper("run_high_priority_dequeued_3rd")
+    )
+    run_normal_priority_dequeued_4th = await storage.create_scraper(
+        _get_scraper("run_normal_priority_dequeued_4th")
+    )
+    for scraper, priority in [
+        (run_normal_priority_dequeued_4th, ScraperRunPriority.NORMAL),
+        (run_high_priority_dequeued_2nd, ScraperRunPriority.HIGH),
+        (run_high_priority_dequeued_3rd, ScraperRunPriority.HIGH),
+        (run_utmost_priority_dequeued_1st, ScraperRunPriority.UTMOST),
+    ]:
+        await queue.enqueue(scraper.id, priority)
+    expected_dequeud_scrapers = [
+        "run_utmost_priority_dequeued_1st",
+        "run_high_priority_dequeued_2nd",
+        "run_high_priority_dequeued_3rd",
+        "run_normal_priority_dequeued_4th",
+        None,
+    ]
+    dequeued = [await queue.dequeue() for _ in range(len(expected_dequeud_scrapers))]
+    dequeued = [item.scraper.name if item is not None else None for item in dequeued]
+    assert dequeued == expected_dequeud_scrapers
