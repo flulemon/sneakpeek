7629b7e2c152352d404d998780602aad8d64580f
Dan Yazovsky <daniil.yazovsky@gmail.com>
2022-07-25 10:43:10 +0100

    Updating storage tests

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -2,6 +2,7 @@ import asyncio
 from datetime import datetime, timedelta
 
 import pytest
+from fakeredis import AsyncFakeRedis
 from pytest_lazyfixture import lazy_fixture
 
 from sneakpeek.lib.models import (
@@ -12,13 +13,12 @@ from sneakpeek.lib.models import (
     ScraperRunStatus,
     ScraperSchedule,
 )
-from sneakpeek.lib.storage.base import (
-    ScraperNotFoundError,
-    ScraperRunPingFinishedError,
-    ScraperRunPingNotStartedError,
-    Storage,
-)
+from sneakpeek.lib.storage.base import ScraperNotFoundError, Storage
 from sneakpeek.lib.storage.in_memory_storage import InMemoryStorage
+from sneakpeek.lib.storage.redis_storage import RedisStorage
+
+# from redis.asyncio import Redis
+
 
 NON_EXISTENT_SCRAPER_ID = 10001
 
@@ -28,19 +28,30 @@ def in_memory_storage() -> Storage:
     return InMemoryStorage()
 
 
+@pytest.fixture
+def redis_storage() -> Storage:
+    return RedisStorage(AsyncFakeRedis())
+
+
+storages = [
+    # lazy_fixture(in_memory_storage.__name__),
+    lazy_fixture(redis_storage.__name__),
+]
+
+
 def _get_scraper(name: str, id: int = UNSET_ID) -> Scraper:
     return Scraper(
         id=id,
         name=name,
         schedule=ScraperSchedule.CRONTAB,
         schedule_crontab=f"schedule_{name}",
-        handler="handler_{name}",
-        config="config_{name}",
+        handler=f"handler_{name}",
+        config=f"config_{name}",
     )
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_read_after_write(storage: Storage):
     expected = _get_scraper("test_read_after_write")
     created = await storage.create_scraper(expected)
@@ -56,7 +67,7 @@ async def test_read_after_write(storage: Storage):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_get_scrapers(storage: Storage):
     expected = [_get_scraper(f"test_get_scrapers_{i}", i) for i in range(10)]
     for item in expected:
@@ -67,7 +78,7 @@ async def test_get_scrapers(storage: Storage):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_search_scrapers(storage: Storage):
     name_filter = "test_that_must_be_found"
     last_id = 50
@@ -105,21 +116,21 @@ async def test_search_scrapers(storage: Storage):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_read_non_existent_scraper_throws(storage: Storage):
     with pytest.raises(ScraperNotFoundError):
         await storage.get_scraper(NON_EXISTENT_SCRAPER_ID)
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_maybe_read_non_existent_scraper_returns_none(storage: Storage):
     actual = await storage.maybe_get_scraper(NON_EXISTENT_SCRAPER_ID)
     assert actual is None
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_update_non_existent_scraper_throws(storage: Storage):
     with pytest.raises(ScraperNotFoundError):
         await storage.update_scraper(
@@ -131,14 +142,14 @@ async def test_update_non_existent_scraper_throws(storage: Storage):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_delete_non_existent_scraper_throws(storage: Storage):
     with pytest.raises(ScraperNotFoundError):
         await storage.delete_scraper(NON_EXISTENT_SCRAPER_ID)
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_delete_scraper(storage: Storage):
     created = await storage.create_scraper(_get_scraper("test_delete_scraper"))
     actual = await storage.get_scraper(created.id)
@@ -150,98 +161,7 @@ async def test_delete_scraper(storage: Storage):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
-async def test_scraper_queue(storage: Storage):
-    created_scraper = await storage.create_scraper(_get_scraper("test_scraper_queue"))
-    created_run = await storage.add_scraper_run(
-        ScraperRun(
-            id=UNSET_ID,
-            scraper=created_scraper,
-            status=ScraperRunStatus.PENDING,
-            priority=ScraperRunPriority.UTMOST,
-            created_at=datetime(year=2000, month=12, day=31),
-        )
-    )
-    assert created_run.id >= 0, "Expected scraper run ID to be positive or zero"
-    assert created_run.scraper == created_scraper
-    assert created_run.status == ScraperRunStatus.PENDING
-    actual_run = await storage.get_scraper_runs(created_scraper.id)
-    assert actual_run == [created_run]
-    has_unfinished_scraper_runs = await storage.has_unfinished_scraper_runs(
-        created_scraper.id
-    )
-    assert has_unfinished_scraper_runs is True
-    with pytest.raises(ScraperRunPingNotStartedError):
-        await storage.ping_scraper_run(created_scraper.id, created_run.id)
-    dequeued = await storage.dequeue_scraper_run()
-    assert dequeued.id == created_run.id
-    assert dequeued.status == ScraperRunStatus.STARTED
-    assert await storage.dequeue_scraper_run() is None
-    await storage.ping_scraper_run(dequeued.scraper.id, dequeued.id)
-    dequeued.status = ScraperRunStatus.SUCCEEDED
-    dequeued.finished_at = datetime.utcnow()
-    finished = await storage.update_scraper_run(dequeued)
-    with pytest.raises(ScraperRunPingFinishedError):
-        await storage.ping_scraper_run(finished.scraper.id, finished.id)
-    assert await storage.dequeue_scraper_run() is None
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
-async def test_scraper_priority_queue_dequeue_order(storage: Storage):
-    created_scraper = await storage.create_scraper(_get_scraper("test_scraper_queue"))
-    NORMAL_PRIORITY_DEQUEUED_4 = 1
-    HIGH_PRIORITY_LOWER_ID_DEQUEUED_3 = 2
-    HIGH_PRIORITY_HIGHER_ID_DEQUEUED_2 = 3
-    UTMOST_PRIORITY_DEQUEUED_1 = 4
-    expected_dequeued_ids = [
-        UTMOST_PRIORITY_DEQUEUED_1,
-        HIGH_PRIORITY_LOWER_ID_DEQUEUED_3,
-        HIGH_PRIORITY_HIGHER_ID_DEQUEUED_2,
-        NORMAL_PRIORITY_DEQUEUED_4,
-        None,
-    ]
-    runs = [
-        ScraperRun(
-            id=NORMAL_PRIORITY_DEQUEUED_4,
-            scraper=created_scraper,
-            status=ScraperRunStatus.PENDING,
-            priority=ScraperRunPriority.NORMAL,
-            created_at=datetime(year=2000, month=12, day=31),
-        ),
-        ScraperRun(
-            id=HIGH_PRIORITY_LOWER_ID_DEQUEUED_3,
-            scraper=created_scraper,
-            status=ScraperRunStatus.PENDING,
-            priority=ScraperRunPriority.HIGH,
-            created_at=datetime(year=2000, month=12, day=31),
-        ),
-        ScraperRun(
-            id=HIGH_PRIORITY_HIGHER_ID_DEQUEUED_2,
-            scraper=created_scraper,
-            status=ScraperRunStatus.PENDING,
-            priority=ScraperRunPriority.HIGH,
-            created_at=datetime(year=2000, month=12, day=31),
-        ),
-        ScraperRun(
-            id=UTMOST_PRIORITY_DEQUEUED_1,
-            scraper=created_scraper,
-            status=ScraperRunStatus.PENDING,
-            priority=ScraperRunPriority.UTMOST,
-            created_at=datetime(year=2000, month=12, day=31),
-        ),
-    ]
-    for run in runs:
-        await storage.add_scraper_run(run)
-    dequeued = [
-        await storage.dequeue_scraper_run() for _ in range(len(expected_dequeued_ids))
-    ]
-    dequeued = [item.id if item is not None else None for item in dequeued]
-    assert dequeued == expected_dequeued_ids
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_delete_old_scraper_runs(storage: Storage):
     scraper = await storage.create_scraper(_get_scraper("test_scraper_queue"))
     to_create = 10
@@ -265,15 +185,14 @@ async def test_delete_old_scraper_runs(storage: Storage):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("storage", [lazy_fixture("in_memory_storage")])
+@pytest.mark.parametrize("storage", storages)
 async def test_lease(storage: Storage):
     lease_name_1 = "test_lease_1"
     lease_name_2 = "test_lease_2"
     owner_1 = "owner_id_1"
     owner_2 = "owner_id_2"
-    now = datetime.utcnow()
-    owner_1_acquire_until = now + timedelta(seconds=1)
-    owner_2_acquire_until = now + timedelta(seconds=5)
+    owner_1_acquire_until = timedelta(seconds=1)
+    owner_2_acquire_until = timedelta(seconds=5)
 
     # initial acquire
     assert (
