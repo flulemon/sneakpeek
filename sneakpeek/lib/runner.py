9bc9fd5f2615812017a7f5c35c336cf9a27bf268
Dan Yazovsky <daniil.yazovsky@gmail.com>
2022-10-05 09:39:57 +0100

    Update runner so it passes context to the scraper implementation

--- sneakpeek/lib/runner.py
+++ sneakpeek/lib/runner.py
@@ -1,10 +1,15 @@
 import logging
 from abc import ABC
 from traceback import format_exc
+from typing import List
 
-from sneakpeek.lib.models import ScraperRun, ScraperRunStatus
+from sneakpeek.config import ScraperConfig
+from sneakpeek.context import ScraperContext
+from sneakpeek.lib.errors import UnknownScraperHandlerError
+from sneakpeek.lib.models import Scraper, ScraperRun, ScraperRunStatus
 from sneakpeek.lib.queue import QueueABC
 from sneakpeek.lib.storage.base import Storage
+from sneakpeek.scraper import ScraperABC
 
 
 class RunnerABC(ABC):
@@ -13,17 +18,40 @@ class RunnerABC(ABC):
 
 
 class Runner(RunnerABC):
-    def __init__(self, queue: QueueABC, storage: Storage) -> None:
+    def __init__(
+        self,
+        handlers: List[Scraper],
+        queue: QueueABC,
+        storage: Storage,
+    ) -> None:
         self._logger = logging.getLogger(__name__)
+        self._handlers = {handler.name: handler for handler in handlers}
         self._queue = queue
         self._storage = storage
+
+    def _get_handler(self, run: ScraperRun) -> ScraperABC:
+        if run.scraper.handler not in self._handlers:
+            raise UnknownScraperHandlerError(
+                f"Unknown scraper handler '{run.scraper.handler}'"
+            )
+        return self._handlers[run.scraper.handler]
+
+    def _build_context(self, run: ScraperRun) -> ScraperContext:
+        return ScraperContext(
+            run.scraper.id,
+            run.id,
+            ScraperConfig.parse_raw(run.scraper.config),
+        )
 
     async def run(self, run: ScraperRun) -> None:
         self._logger.info(f"Starting executing {run}")
         try:
             await self._queue.ping_scraper_run(run.scraper.id, run.id)
+            run.scraper.config
+            handler = self._get_handler(run)
+            context = self._build_context(run)
+            run.result = handler.run(context, context.config.scraper_params)
             run.status = ScraperRunStatus.SUCCEEDED
-            run.result = "Successfully finished"
         except Exception as e:
             self._logger.error(f"Failed to execute: {run}: {e}")
             self._logger.debug(f"Failed to execute: {run}. Traceback: {format_exc()}")
