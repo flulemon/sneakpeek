b7d489995b368712c06debfeccb7c625ee216d54
Dan Yazovsky <daniil.yazovsky@gmail.com>
2022-11-16 07:55:03 +0000

    Adding configuring logging

---
+++ sneakpeek/logging.py
@@ -0,0 +1,46 @@
+import logging
+from contextlib import contextmanager
+from contextvars import ContextVar
+
+from sneakpeek.lib.models import ScraperRun
+
+ctx_scraper_run = ContextVar("scraper_run")
+
+
+@contextmanager
+def scraper_run_context(scraper_run: ScraperRun) -> None:
+    try:
+        token = ctx_scraper_run.set(scraper_run)
+        yield
+    finally:
+        ctx_scraper_run.reset(token)
+
+
+class ScraperContextInjectingFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        scraper_run: ScraperRun = ctx_scraper_run.get(None)
+        record.scraper_run_human_name = "-"
+        if scraper_run:
+            record.scraper_run_id = scraper_run.id
+            scraper = scraper_run.scraper
+            if scraper:
+                record.scraper_id = scraper.id
+                record.scraper_name = scraper.name
+                record.scraper_handler = scraper.handler
+                record.scraper_run_human_name = (
+                    f"{scraper.name}::{scraper.id}::{scraper_run.id}"
+                )
+        return True
+
+
+def configure_logging(level: int = logging.INFO):
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d][%(scraper_run_human_name)s] - %(message)s"
+        )
+    )
+    handler.addFilter(ScraperContextInjectingFilter())
+    logger.addHandler(handler)
+    logger.setLevel(level)
