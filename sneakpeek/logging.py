8e969c3f6589612ccd839b762d24d30f1c7969a0
Dan Yazovsky <daniil.yazovsky@gmail.com>
2023-03-04 12:02:01 +0000

    Adding logging docs

--- sneakpeek/logging.py
+++ sneakpeek/logging.py
@@ -9,6 +9,13 @@ ctx_scraper_run = ContextVar("scraper_run")
 
 @contextmanager
 def scraper_run_context(scraper_run: ScraperRun) -> None:
+    """
+    Initialize scraper run logging context which automatically adds
+    scraper and scraper run IDs to the logging metadata
+
+    Args:
+        scraper_run (ScraperRun): Scraper run definition
+    """
     try:
         token = ctx_scraper_run.set(scraper_run)
         yield
@@ -17,7 +24,35 @@ def scraper_run_context(scraper_run: ScraperRun) -> None:
 
 
 class ScraperContextInjectingFilter(logging.Filter):
+    """
+    Scraper context filter which automatically injects
+    scraper and scraper run IDs to the logging metadata.
+
+    Example of usage:
+
+    .. code-block:: python3
+
+        logger = logging.getLogger()
+        handler = logging.StreamHandler()
+        handler.addFilter(ScraperContextInjectingFilter())
+        logger.addHandler(handler)
+    """
+
     def filter(self, record: logging.LogRecord) -> bool:
+        """Injects scraper metadata into log record:
+
+        * ``scraper_run_id`` - Scraper Run ID
+        * ``scraper_id`` - Scraper ID
+        * ``scraper_name`` - Scraper name
+        * ``scraper_handler`` - Scraper logic implementation
+        * ``scraper_run_human_name`` - Formatted scraper run ID (``<name>::<scraper_id>::<scraper_run_id>``)
+
+        Args:
+            record (logging.LogRecord): Log record to inject metadata into
+
+        Returns:
+            bool: Always True
+        """
         scraper_run: ScraperRun = ctx_scraper_run.get(None)
         record.scraper_run_human_name = "-"
         if scraper_run:
@@ -34,6 +69,16 @@ class ScraperContextInjectingFilter(logging.Filter):
 
 
 def configure_logging(level: int = logging.INFO):
+    """
+    Helper function to configure logging:
+
+    * Adds console logger to the root logger
+    * Adds scraper context injector filter to the console logger
+    * Configures console formatting to use scraper metadata
+
+    Args:
+        level (int, optional): Minimum logging level. Defaults to logging.INFO.
+    """
     logger = logging.getLogger()
     handler = logging.StreamHandler()
     handler.setFormatter(
