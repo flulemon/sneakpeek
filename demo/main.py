706a5dfba8dd4ad5caff939701d5d3b15488a236
Dan Yazovsky <daniil.yazovsky@gmail.com>
2023-03-16 09:30:20 +0000

    Updated demo

--- demo/main.py
+++ demo/main.py
@@ -2,8 +2,12 @@ import random
 
 from demo_scraper import DemoScraper
 
-from sneakpeek.lib.models import Scraper, ScraperRunPriority, ScraperSchedule
-from sneakpeek.lib.storage.in_memory_storage import InMemoryStorage
+from sneakpeek.lib.models import Scraper, ScraperJobPriority, ScraperSchedule
+from sneakpeek.lib.storage.in_memory_storage import (
+    InMemoryLeaseStorage,
+    InMemoryScraperJobsStorage,
+    InMemoryScrapersStorage,
+)
 from sneakpeek.logging import configure_logging
 from sneakpeek.plugins.rate_limiter_plugin import (
     RateLimiterPlugin,
@@ -26,28 +30,33 @@ urls = [
     "https://linkedin.com",
 ]
 
+scrapers = [
+    Scraper(
+        id=id,
+        name=f"Demo Scraper ({url})",
+        schedule=ScraperSchedule.EVERY_MINUTE,
+        handler=DemoScraper().name,
+        config=ScraperConfig(params={"url": url}),
+        schedule_priority=random.choice(
+            [
+                ScraperJobPriority.HIGH,
+                ScraperJobPriority.UTMOST,
+                ScraperJobPriority.NORMAL,
+            ]
+        ),
+    )
+    for id, url in enumerate(urls)
+]
+
+scrapers_storage = InMemoryScrapersStorage(scrapers)
+jobs_storage = InMemoryScraperJobsStorage()
+lease_storage = InMemoryLeaseStorage()
+
 server = SneakpeekServer(
     handlers=[DemoScraper()],
-    storage=InMemoryStorage(
-        [
-            Scraper(
-                id=id,
-                name=f"Demo Scraper ({url})",
-                schedule=ScraperSchedule.EVERY_MINUTE,
-                handler=DemoScraper().name,
-                config=ScraperConfig(params={"url": url}),
-                schedule_priority=random.choice(
-                    [
-                        ScraperRunPriority.HIGH,
-                        ScraperRunPriority.UTMOST,
-                        ScraperRunPriority.NORMAL,
-                    ]
-                ),
-            )
-            for id, url in enumerate(urls)
-        ],
-        is_read_only=True,
-    ),
+    scrapers_storage=scrapers_storage,
+    jobs_storage=jobs_storage,
+    lease_storage=lease_storage,
     plugins=[
         RequestsLoggingPlugin(),
         RobotsTxtPlugin(),
